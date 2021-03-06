25a32b9f3546ee8225b5aaea32f62a2f40a1789b
Mustafa <codingbox@outlook.com>
2021-03-06 17:36:20 -0500

    ReentrantLock() implemented for locking

--- src/shubbar/ca/BankAccount.java
+++ src/shubbar/ca/BankAccount.java
@@ -1,5 +1,9 @@
 package shubbar.ca;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Mustafa <codingbox@outlook.com>
  * Created at 2021-03-06
@@ -8,9 +12,13 @@ public class BankAccount {
     private double balance;
     private String accountNumber;
 
+    private Lock lock;
+
     public BankAccount(String accountNumber, double balance) {
         this.balance = balance;
         this.accountNumber = accountNumber;
+
+        this.lock = new ReentrantLock();
     }
 
 
@@ -23,16 +31,63 @@ public class BankAccount {
 //    }
 
     public void deposit(double amount) {
-        synchronized (this) {
-            balance += amount;
+
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try{
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch(InterruptedException e) {
+            // do something
         }
-        balance += amount;
+
+
+
+
+/* Method 2 */
+//        lock.lock();
+//        try {
+//            balance += amount;
+//        } finally {
+//            lock.unlock();
+//        }
+
+
+/* Method 1 */
+//        synchronized (this) {
+//            balance += amount;
+//        }
+//        balance += amount;
     }
 
+
     public void withdraw(double amount) {
-        synchronized (this) {
-            balance -= amount;
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock.");
+            }
+        } catch (InterruptedException e) {
+            // do something
         }
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void printAccountNumber(){
+        System.out.println("Account number = " + accountNumber);
     }
 
 }
